68b2f09cd46dc51f07725901d8b0be13349df59d
Dinesh Pallapa <dinesh@enroco.com>
2017-12-23 22:12:12 +0530

    Fixed issue with note not found exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  rescue_from ActiveRecord::RecordNotFound, with: :resource_not_found
+
+  protected
+
+  def resource_not_found
+    flash[:alert] = "The note you are looking for could not be found"
+    redirect_to root_path
+  end
 end
